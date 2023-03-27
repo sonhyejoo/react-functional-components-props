dcca348f4949f9fc83f07c725bd35084f6b24546
Son Hyejoo <seongwoo.cho@gmail.com>
2023-03-27 19:57:27 +0000

    baseStats table finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Showcase from './Showcase';
-import './App.css';
+import Showcase from "./Showcase";
+import BaseStats from "./BaseStats";
+import "./App.css";
 
 function App() {
+  const baseStats = {
+    hp: 45,
+    attack: 49,
+    defense: 65,
+    spAttack: 65,
+    spDef: 65,
+    speed: 45,
+  };
+
   return (
-    <div className='main-wrapper background'>
+    <div className="main-wrapper background">
       <Showcase />
+      <BaseStats stats={baseStats} />
     </div>
   );
 }
